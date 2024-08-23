9d53896b5c63dbb2cfe62fc185d525dcd02d257c
Mohammed Ajsal <ajsal12aju@gmail.com>
2024-08-23 23:04:55 +0530

    started

---
+++ 1stmodule.js
@@ -0,0 +1,29 @@
+// 1. sum of the natural number 
+
+function sunOfNaturalNumbers(num){
+    let sum= 0;
+    for (let i = 0; i <= num; i++){
+        sum =sum +1
+    }
+    // return sum
+    // another methord
+    return num*(num+1)/2;
+    // behind the sence its happenig 
+    // Sum= 5 * (5+1) / 2 ;
+        //  6*5 = 30 /2 = 15
+
+    }
+console.log(sunOfNaturalNumbers(5))
+
+// Q2 sum of the digits of a number 1234 ans 1+2+3+4 like
+
+function digitCalculation(num) {
+let sum= 0;
+while (num > 0) {
+    sum += num%10;
+    num = Math.floor(num/10)
+}
+return sum
+}
+
+console.log(digitCalculation(12));
